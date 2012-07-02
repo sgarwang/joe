7c0fd9222d8b038c83eebc5c88af19f0f2fac6d6
sgarwang <sgarwang@dvmteki-MacBook-Pro.local>
2012-07-02 11:41:05 +0800

    rails new joe, bundle install and rails s

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joe::Application.config.secret_token = 'f52452d1f7ba69fbde147e6897092e8f54a0b80f14cd222137e6b5065bdce229ef5e67929cc42ed05cda1664bcd07b2aade8bacb6f534145a8ca697827b52e86'
