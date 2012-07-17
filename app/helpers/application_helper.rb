90af0fd61385b210510863620dfc3b0d85f13a16
sgarwang <sgarwang@gmail.com>
2012-07-17 15:37:11 +0800

    add Form and UseDefined Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def gravatar_url(email)
+    debugger
+    gravatar_email = Digest::MD5.hexdigest(email.downcase)
+    return "http://www.gravatar.com/avatar/#{gravatar_email}?s=48"
+  end
+    
 end
